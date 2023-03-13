677d06ee8e42b1df2c759e0b4b1f3d18c44ea497
wern0531 <wern780531@hotmail.com>
2023-03-13 20:40:22 +0800

    deploy

---
+++ assets/CartView-9bd845ea.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as _}from"./index-7fde9863.js";const t={};function n(o,a){return _(),c("div",null," 這是購物車 ")}const s=e(t,[["render",n],["__scopeId","data-v-cd4f68cb"]]);export{s as default};
--- assets/DeckView-bd99a12f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as _,o as c}from"./index-1a98ff12.js";const t={};function n(o,a){return c(),_("div",null," 這是牌組介紹 ")}const d=e(t,[["render",n],["__scopeId","data-v-e57dade0"]]);export{d as default};
