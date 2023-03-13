7761433da6f01055fa6ac6fa6fd136c9f19f5a51
wern0531 <wern780531@hotmail.com>
2023-03-13 20:15:16 +0800

    deploy

---
+++ assets/CartView-4aafb02e.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as _}from"./index-1a98ff12.js";const t={};function n(o,a){return _(),c("div",null," 這是購物車 ")}const s=e(t,[["render",n],["__scopeId","data-v-cd4f68cb"]]);export{s as default};
