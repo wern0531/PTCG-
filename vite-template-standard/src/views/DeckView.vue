7aa5a8898b76fd9eb6ce834ec2cec6e4b81d7d13
wern0531 <wern780531@hotmail.com>
2023-04-08 15:18:36 +0800

    牌組頁面及連結修正

--- vite-template-standard/src/views/DeckView.vue
+++ vite-template-standard/src/views/DeckView.vue
@@ -4,61 +4,103 @@
       <img src="../assets/image/pikachu_gif.gif" alt="" />
     </div>
   </LoadingItem>
-  <div class="container p-3" v-if="isready">
-    <div class="d-flex flex-column flex-lg-row" style="margin-top: 63px;">
-      <div>
-        <img class="deckImg"  :src="article.image" alt="" />
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col col-lg-9">
+        <div class="row">
+          <div
+            class="col-0 col-sm-6 col-lg mb-2 mb-lg-0 d-flex justify-content-start justify-content-lg-center"
+            v-for="(item, index) in deckmenu"
+            :key="index"
+          >
+            <button class="btn btn-myColor"
+            @click="getDeck(item)">
+              {{ item }}
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="ms-3 text-start text-white d-flex flex-column justify-content-center">
-        <span class="mb-3">{{ article.description }}</span>
-        <h1 class="mb-3 mb-md-5">{{ article.title }}</h1>
-        <div>
-            <div class="deckList d-flex justify-content-between flex-column">
-                <div class="d-flex mb-1">
-                  <div class="d-flex text-white">牌組強度</div>
-                  <div class="d-flex align-items-center text-white ms-3">
-                    <i
-                      v-for="index in 5"
-                      :key="index"
-                      :class="{
-                        'fa-solid fa-star': index <= starLevel,
-                        'fa-regular fa-star': index > starLevel,
-                      }"
-                    ></i>
-                  </div>
-                </div>
-                <div class="d-flex mb-1">
-                  <div class="d-flex text-white">操作難度</div>
-                  <div class="d-flex align-items-center text-white ms-3">
-                    <i
-                      v-for="index in 5"
-                      :key="index"
-                      :class="{
-                        'fa-solid fa-star': index <= starLevel,
-                        'fa-regular fa-star': index > starLevel,
-                      }"
-                    ></i>
-                  </div>
-                </div>
-                <div class="d-flex">
-                  <div class="d-flex text-white">構築價格</div>
-                  <div class="d-flex align-items-center text-white ms-3">
-                    <i
-                      v-for="index in 5"
-                      :key="index"
-                      :class="{
-                        'fa-solid fa-star': index <= starLevel,
-                        'fa-regular fa-star': index > starLevel,
-                      }"
-                    ></i>
-                  </div>
-                </div>
-              </div>
+      <div class="col col-lg-3 d-flex justify-content-end">
+        <div class="dropdown">
+          <button
+            class="btn btn-light dropdown-toggle text-dark"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            選擇牌組
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li v-for="(item, index) in deckmenu" :key="index">
+              <a class="dropdown-item" @click="getDeck(item)">
+                {{ item }}
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-    <div v-html="article.content" class="mt-3 p-3 text-white text-start border border-white">
+  </div>
+  <div class="container p-3" v-if="isready">
+    <div class="d-flex flex-column flex-lg-row">
+      <div>
+        <img class="deckImg" :src="article.image" alt="" />
+      </div>
+      <div
+        class="ms-3 text-start text-white d-flex flex-column justify-content-center"
+      >
+        <span class="mb-3">{{ article.description }}</span>
+        <h1 class="mb-3 mb-md-5">{{ article.title }}</h1>
+        <div>
+          <div class="deckList d-flex justify-content-between flex-column">
+            <div class="d-flex mb-1">
+              <div class="d-flex text-white">牌組強度</div>
+              <div class="d-flex align-items-center text-white ms-3">
+                <i
+                  v-for="index in 5"
+                  :key="index"
+                  :class="{
+                    'fa-solid fa-star': index <= starLevel,
+                    'fa-regular fa-star': index > starLevel,
+                  }"
+                ></i>
+              </div>
+            </div>
+            <div class="d-flex mb-1">
+              <div class="d-flex text-white">操作難度</div>
+              <div class="d-flex align-items-center text-white ms-3">
+                <i
+                  v-for="index in 5"
+                  :key="index"
+                  :class="{
+                    'fa-solid fa-star': index <= starLevel,
+                    'fa-regular fa-star': index > starLevel,
+                  }"
+                ></i>
+              </div>
+            </div>
+            <div class="d-flex">
+              <div class="d-flex text-white">構築價格</div>
+              <div class="d-flex align-items-center text-white ms-3">
+                <i
+                  v-for="index in 5"
+                  :key="index"
+                  :class="{
+                    'fa-solid fa-star': index <= starLevel,
+                    'fa-regular fa-star': index > starLevel,
+                  }"
+                ></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div
+      v-html="article.content"
+      class="mt-3 p-3 text-white text-start border border-white"
+    ></div>
   </div>
 </template>
 
@@ -72,15 +114,25 @@ export default {
       article: {},
       starLevel: 4,
       title: '',
-      id: ''
+      id: '',
+      deckmenu: []
     }
   },
   methods: {
-    getArticles () {
+    getArticles (first = true) {
       this.$http
         .get(`${VITE_URL}v2/api/${VITE_PATH}/articles`)
         .then((res) => {
-          res.data.articles.forEach(item => {
+          if (first === true) {
+            const deckdata = res.data.articles
+            deckdata.forEach((item) => {
+              if (item.tag === '牌組') {
+                this.deckmenu.push(item.title)
+                console.log(this.deckmenu)
+              }
+            })
+          }
+          res.data.articles.forEach((item) => {
             if (item.title === this.title) {
               this.id = item.id
             }
@@ -102,6 +154,12 @@ export default {
         .catch((err) => {
           alert(err.response.data.message)
         })
+    },
+    getDeck (title) {
+      this.isLoading = true
+      this.title = title
+      // this.$route.params.name = this.title
+      this.getArticles(false)
     }
   },
   mounted () {
@@ -113,18 +171,18 @@ export default {
 </script>
 
 <style scoped>
-.container{
-    max-width: 900px;
-    flex-grow: 1;
+.container {
+  max-width: 900px;
+  flex-grow: 1;
 }
-.deckImg{
+.deckImg {
   width: 600px;
 }
-.deckImg:hover{
+.deckImg:hover {
   transform: scale(1.15);
 }
-@media (max-width: 768px){
-  .deckImg{
+@media (max-width: 768px) {
+  .deckImg {
     width: 343px;
   }
 }
